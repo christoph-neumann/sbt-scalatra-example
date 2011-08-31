7e62a148d1991987f92387fe366850ece6a770ee
Christoph Neumann <christoph@neumannhaus.com>
2011-08-31 17:15:49 -0500

    run.sh sets up the classpath and runs the app.

--- src/main/scripts/run.sh
+++ src/main/scripts/run.sh
@@ -1,4 +1,15 @@
 #!/bin/bash
 
+# Find the application home directory and switch to it
 app_home=$(cd `dirname "$0"`; pwd)
-echo "$app_home"
+cd "$app_home"
+
+# Options to pass to the JVM
+java_opts="-server -Xmx256m -XX:MaxPermSize=128m"
+
+# Find all the jars and put them on the classpath
+for jar in `ls ${app_home}/lib/*.jar`; do
+	classpath="$classpath:$jar"
+done
+
+java $java_opts -cp "$classpath" app.Main $*
